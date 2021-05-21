44052fe33ae349eeb1d3e2da7d495fc2eda02de0
Anthony Suárez <neolight1010@gmail.com>
2021-05-21 01:28:19 -0500

    Set data parser up.

--- lib/routes/bio/bio.dart
+++ lib/routes/bio/bio.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:neo_site/routes/bio/_data.dart';
 import 'package:neo_site/utils/widgets.dart';
 import 'package:neo_site/utils/pages.dart';
+import 'package:yaml/yaml.dart';
 import '../index.dart';
+
+Future<dynamic> _loadData() async {
+  final _yamlStr = await rootBundle.loadString("data/bio.yaml");
+  return loadYaml(_yamlStr);
+}
 
 class BioPage extends StatefulWidget {
   static String route = '/bio/';
@@ -12,54 +19,65 @@ class BioPage extends StatefulWidget {
 
 class _BioPageState extends State<BioPage> {
   String _description = Data.description;
+  final data = _loadData();
 
   @override
   Widget build(BuildContext context) {
-    return DefaultPage(
-      backPageRoute: IndexPage.route,
-      title: "bio",
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-            child: Column(
-              children: [
-                Text(_description),
-                Divider(
-                  height: 50,
+    return FutureBuilder(
+        future: _loadData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data;
+
+            return DefaultPage(
+              backPageRoute: IndexPage.route,
+              title: data["title"],
+              body: SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    child: Column(
+                      children: [
+                        Text(_description),
+                        Divider(
+                          height: 50,
+                        ),
+                        DoubleRowTitleList(title: 'Certificates', list: [
+                          'freeCodeCamp Full Stack Dev. (x/y)',
+                          'IBM/Coursera Data Science Professional',
+                          'Climatón EC finalist'
+                        ]),
+                        Divider(
+                          height: 50,
+                        ),
+                        DoubleRowTitleList(
+                          title: 'Programming/Markup Languages',
+                          list: [
+                            'Python',
+                            'Javascript',
+                            'Dart',
+                            'C++',
+                            'HTML/CSS',
+                          ],
+                        ),
+                        Divider(
+                          height: 50,
+                        ),
+                        DoubleRowTitleList(title: "Developer Skills", list: [
+                          "Git",
+                          "Django",
+                          "Databases (Document-Oriented, Relational)",
+                          "APIs (REST, GraphQL)",
+                        ])
+                      ],
+                    ),
+                  ),
                 ),
-                DoubleRowTitleList(title: 'Certificates', list: [
-                  'freeCodeCamp Full Stack Dev. (x/y)',
-                  'IBM/Coursera Data Science Professional',
-                  'Climatón EC finalist'
-                ]),
-                Divider(
-                  height: 50,
-                ),
-                DoubleRowTitleList(
-                  title: 'Programming/Markup Languages',
-                  list: [
-                    'Python',
-                    'Javascript',
-                    'Dart',
-                    'C++',
-                    'HTML/CSS',
-                  ],
-                ),
-                Divider(
-                  height: 50,
-                ),
-                DoubleRowTitleList(title: "Developer Skills", list: [
-                  "Git",
-                  "Django",
-                  "Databases (Document-Oriented, Relational)",
-                  "APIs (REST, GraphQL)",
-                ])
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
